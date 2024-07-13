8e0ce27a710ffca2961eec21368bc40e4afec621
Jack Herrington <jherr@pobox.com>
2024-07-13 06:27:49 -0700

    Small refactoring

--- src/movies/index.ts
+++ src/movies/index.ts
@@ -1,10 +1,69 @@
 'use server'
 import { getPayloadHMR } from '@payloadcms/next/utilities'
 import configPromise from '@payload-config'
-
-import { posterURL } from './utils'
 import { revalidatePath } from 'next/cache'
 
+import { posterURL } from './utils'
+
+export async function searchMovies(query: string) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+      query,
+    )}&include_adult=false&language=en-US&page=1&api_key=${process.env.TMDB_API_KEY}`,
+  )
+  const { results } = await response.json()
+  return results
+    .map(({ id, poster_path, title }: { id: string; poster_path: string; title: string }) => ({
+      id,
+      poster_path,
+      title,
+    }))
+    .filter(({ poster_path }: { poster_path: string }) => !!poster_path)
+}
+
+export async function addMovieAction(movieId: number) {
+  const payload = await getPayloadHMR({ config: configPromise })
+
+  const movieDataReq = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&api_key=${process.env.TMDB_API_KEY}`,
+  )
+  const { title, poster_path, overview, tagline, genres: genreObjects } = await movieDataReq.json()
+  const genres = genreObjects.map(({ name }: { name: string }) => ({ name }))
+
+  const response = await fetch(posterURL(poster_path))
+  const arrayBuffer = await response.arrayBuffer()
+  const posterBuffer = Buffer.from(arrayBuffer)
+
+  const posterMedia = await payload.create({
+    collection: 'media',
+    data: {
+      text: `${title} Poster`,
+    },
+    file: {
+      data: posterBuffer,
+      name: `${movieId}.jpg`,
+      mimetype: 'image/jpeg',
+      size: posterBuffer.byteLength,
+    },
+  })
+
+  const movie = await payload.create({
+    collection: 'movies',
+    data: {
+      name: title,
+      url: `https://www.themoviedb.org/movie/${movieId}`,
+      votes: 0,
+      poster: posterMedia.id,
+      overview,
+      tagline,
+      genres,
+    },
+  })
+
+  revalidatePath('/')
+
+  return movie
+}
 
 export async function addVote(movieId: number) {
   const payload = await getPayloadHMR({ config: configPromise })
@@ -27,70 +86,4 @@ export async function addVote(movieId: number) {
     sort: '-votes',
   })
   return movies.docs
-}
-
-export async function addMovieAction(movieId: number) {
-  const payload = await getPayloadHMR({ config: configPromise })
-
-  const movieDataReq = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&api_key=${process.env.TMDB_API_KEY}`,
-  )
-  const { title, poster_path, overview, tagline, genres: genreObjects } = await movieDataReq.json()
-  const genres = genreObjects.map(({ name }: { name: string }) => ({ name }))
-
-  const response = await fetch(posterURL(poster_path))
-  const arrayBuffer = await response.arrayBuffer()
-  const posterBuffer = Buffer.from(arrayBuffer)
-
-  const posterMedia = await payload.create({
-    collection: 'media',
-    data: {
-      text: `${title} Poster`,
-    },
-    overrideAccess: true,
-    showHiddenFields: false,
-    disableVerificationEmail: true,
-    // Alternatively you can use a local filepath if you have local media
-    file: {
-      data: posterBuffer,
-      name: `${movieId}.jpg`,
-      mimetype: 'image/jpeg',
-      size: posterBuffer.byteLength,
-    },
-  })
-
-  const movie = await payload.create({
-    collection: 'movies',
-    data: {
-      name: title,
-      url: `https://www.themoviedb.org/movie/${movieId}`,
-      votes: 0,
-      poster: posterMedia.id,
-      overview,
-      tagline,
-      genres,
-    },
-    overrideAccess: true,
-    showHiddenFields: false,
-  })
-
-  revalidatePath('/')
-
-  return movie
-}
-
-export async function searchMovies(query: string) {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
-      query,
-    )}&include_adult=false&language=en-US&page=1&api_key=${process.env.TMDB_API_KEY}`,
-  )
-  const { results } = await response.json()
-  return results
-    .map(({ id, poster_path, title }: { id: string; poster_path: string; title: string }) => ({
-      id,
-      poster_path,
-      title,
-    }))
-    .filter(({ poster_path }: { poster_path: string }) => !!poster_path)
 }
